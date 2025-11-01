ed6bc3e9a9b2ca4212966b8607ad7d56f2504825
MateusDaroz <matlicoop@gmail.com>
2025-11-01 19:45:42 -0300

    add: custom alert dialog code

--- lib/features/auth/view/widgets/custom_alert_dialog.dart
+++ lib/features/auth/view/widgets/custom_alert_dialog.dart
@@ -1,10 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CustomAlertDialog extends StatelessWidget {
-  const CustomAlertDialog({super.key});
+  final String title;
+  final String content;
+
+  const CustomAlertDialog({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      backgroundColor: Color.fromRGBO(25, 24, 29, 1),
+      title: Text(title, style: TextStyle(color: Colors.white),),
+      content: Text(content, style: TextStyle(color: const Color.fromRGBO(255, 255, 255, .75)),),
+      actions: <Widget>[
+        TextButton(
+          style: TextButton.styleFrom(textStyle: Theme.of(context).textTheme.labelLarge),
+          child: Text('Entendi'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
   }
 }
